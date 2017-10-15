17fc66fe5c14500f98345fcdeb2a31136be96176
Plasmoxy <sebohacker@gmail.com>
2017-10-15 22:46:34 +0200

    color detector

--- opencv2/colorobj.py
+++ opencv2/colorobj.py
@@ -1,18 +1,18 @@
 import cv2
 import numpy as np
 
-lowerBound=np.array([0,106,178])
-upperBound=np.array([0,36,255])
+lowerBound=np.array([33,80,50])
+upperBound=np.array([102,255,255])
 
 cam= cv2.VideoCapture(0)
 kernelOpen=np.ones((5,5))
 kernelClose=np.ones((20,20))
 
-#font=cv2.InitFont(cv2.cv.CV_FONT_HERSHEY_SIMPLEX,2,0.5,0,3,1)
+font = cv2.FONT_HERSHEY_SIMPLEX
 
 while True:
     ret, img=cam.read()
-    img=cv2.resize(img,(340,220))
+    #img=cv2.resize(img,(340,220))
 
     #convert BGR to HSV
     imgHSV= cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
@@ -28,8 +28,8 @@ while True:
     cv2.drawContours(img,conts,-1,(255,0,0),3)
     for i in range(len(conts)):
         x,y,w,h=cv2.boundingRect(conts[i])
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255), 2)
-        #cv2.PutText(cv2.fromarray(img), str(i+1),(x,y+h),font,(0,255,255))
+        cv2.rectangle(img,(x,y),(x+w,y+h),(204, 255, 0), 2)
+        cv2.putText(img, "{} ({},{})".format(i+1, x, y),(x-20,y+h+20),font, 0.7,(0, 0, 255), 2)
     cv2.imshow("maskClose",maskClose)
     cv2.imshow("maskOpen",maskOpen)
     cv2.imshow("mask",mask)
