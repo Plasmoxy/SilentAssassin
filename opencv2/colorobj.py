ff3cb6f8d60cef382ce512a4ff7c7659b7b0b352
Plasmoxy <31391787+Plasmoxy@users.noreply.github.com>
2017-10-16 15:46:54 +0200

    sum chng

--- opencv2/colorobj.py
+++ opencv2/colorobj.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
 
-lowerBound=np.array([33,80,50])
-upperBound=np.array([102,255,255])
+lowerBound=np.array([45,100,70])
+upperBound=np.array([90,255,255])
 
 cam= cv2.VideoCapture(0)
 kernelOpen=np.ones((5,5))
@@ -10,7 +10,9 @@ kernelClose=np.ones((20,20))
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-while True:
+reasonExit=True
+
+while True and reasonExit:
     ret, img=cam.read()
     #img=cv2.resize(img,(340,220))
 
@@ -24,12 +26,16 @@ while True:
 
     maskFinal=maskClose
     _,conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+
+    cv2.rectangle(img, (0, 0), (40, 40), (255, 0, 255), 2)
     
     cv2.drawContours(img,conts,-1,(255,0,0),3)
     for i in range(len(conts)):
         x,y,w,h=cv2.boundingRect(conts[i])
         cv2.rectangle(img,(x,y),(x+w,y+h),(204, 255, 0), 2)
         cv2.putText(img, "{} ({},{})".format(i+1, x, y),(x-20,y+h+20),font, 0.7,(0, 0, 255), 2)
+        if (x > 0 and x < 40 and y > 0 and y < 40):
+            reasonExit = False
     cv2.imshow("maskClose",maskClose)
     cv2.imshow("maskOpen",maskOpen)
     cv2.imshow("mask",mask)
--- opencv2/facetests/webfacetest.py
+++ opencv2/facetests/webfacetest.py
@@ -1,11 +1,13 @@
 import cv2
+import time
 
 cap = cv2.VideoCapture(0)
-cap.set(3, 1920) #WIDTH
-cap.set(4, 1080) #HEIGHT
+gpath='D:/Pmxy/GIT/SilentAssassin'
 
-face_cascade = cv2.CascadeClassifier('S:\\GIT\\SilentAssassin\\opencv2\\haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('S:\\GIT\\SilentAssassin\\opencv2\\haarcascade_eye.xml')
+open(gpath+'/opencv2/facetests/haarcascade_frontalface_default.xml')
+
+face_cascade = cv2.CascadeClassifier(gpath+'/opencv2/facetests/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(gpath+'/opencv2/facetests/haarcascade_eye.xml')
 
 while(True):
     # Capture frame-by-frame
@@ -13,16 +15,16 @@ while(True):
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 2, 2)
-    print(len(faces))
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    #print(len(faces))
     # Display the resulting frame
     for (x,y,w,h) in faces:
          cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
          roi_gray = gray[y:y+h, x:x+w]
          roi_color = frame[y:y+h, x:x+w]
-         eyes = eye_cascade.detectMultiScale(roi_gray)
-         for (ex,ey,ew,eh) in eyes:
-             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+         #eyes = eye_cascade.detectMultiScale(roi_gray)
+         #for (ex,ey,ew,eh) in eyes:
+         #    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
