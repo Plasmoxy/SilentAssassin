4cb70841d5c9f97b7fb5656aea5e275b4125b72a
Plasmoxy <sebohacker@gmail.com>
2018-04-11 19:04:28 +0200

    xd

--- opencv2/colorobj.py
+++ opencv2/colorobj.py
@@ -26,10 +26,12 @@ while True:
     _,conts,h=cv2.findContours(maskFinal.copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     
     cv2.drawContours(img,conts,-1,(255,0,0),3)
+
     for i in range(len(conts)):
         x,y,w,h=cv2.boundingRect(conts[i])
         cv2.rectangle(img,(x,y),(x+w,y+h),(204, 255, 0), 2)
         cv2.putText(img, "{} ({},{})".format(i+1, x, y),(x-20,y+h+20),font, 0.7,(0, 0, 255), 2)
+
     cv2.imshow("maskClose",maskClose)
     cv2.imshow("maskOpen",maskOpen)
     cv2.imshow("mask",mask)
